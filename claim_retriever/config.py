55f7aa51989b22af95a9e83e20ae19a71e075af0
Ghassan-Dib <ghassan.dib.dev@gmail.com>
2025-07-14 14:27:16 +0100

    refactor: claim retriever

--- claim_retriever/config.py
+++ claim_retriever/config.py
@@ -3,9 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Environment variables
 API_KEY = os.getenv("API_KEY")
 FACT_CHECK_TOOLS_URL = os.getenv("FACT_CHECK_TOOLS_URL")
-OUTPUT_FILENAME = "claim_reviews.json"
+
+# Paths to data files
+CLAIMS_PATH = "data/claims.csv"
+QUERIES_PATH = "data/queries.txt"
+
+# Default configurations
 DEFAULT_LANGUAGE = "ar"
 DEFAULT_MAX_AGE_DAYS = 365
 DEFAULT_PAGE_SIZE = 50
